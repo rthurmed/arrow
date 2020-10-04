2ea250411bbc1a03880594417215159d7fcd26ba
Arthur Medeiros <rthurmedeiros@gmail.com>
2020-10-03 23:21:00 -0300

    Bow animation

--- src/archer.lua
+++ src/archer.lua
@@ -6,8 +6,6 @@ function Archer:new(world, x, y)
   local that = {}
 
   that.world = world
-  that.x = x
-  that.y = y
 
   that.h = 72
   that.w = 72
@@ -17,6 +15,12 @@ function Archer:new(world, x, y)
   that.body = love.physics.newBody(that.world, x, y, "dynamic")
   that.shape = love.physics.newRectangleShape(that.w, that.h)
   that.fixture = love.physics.newFixture(that.body, that.shape)
+
+  that.bow = {
+    i0 = love.graphics.newImage('assets/bow0.png'),
+    i50 = love.graphics.newImage('assets/bow50.png'),
+    i100 = love.graphics.newImage('assets/bow100.png')
+  }
 
   that.arrows = {}
   that.fireDelay = 1 -- seconds
@@ -29,13 +33,13 @@ end
 function Archer:updateMovement(dt)
   local mx, my = 0, 0
 
-  if love.keyboard.isDown('w') then my = -1 end
-  if love.keyboard.isDown('s') then my =  1 end
+  -- if love.keyboard.isDown('w') then my = -1 end
+  -- if love.keyboard.isDown('s') then my =  1 end
   if love.keyboard.isDown('a') then mx = -1 end
   if love.keyboard.isDown('d') then mx =  1 end
 
-  self.x = self.x + mx * dt * self.speed
-  self.y = self.y + my * dt * self.speed
+  self.body:setX(self.body:getX() + (mx * dt * self.speed))
+  self.body:setY(self.body:getY() + (my * dt * self.speed))
 end
 
 function Archer:updateShooting(dt)
@@ -55,7 +59,10 @@ function Archer:updateShooting(dt)
 
   if self.fireStrength > 0 and not love.mouse.isDown(1) then
     local mousex, mousey = love.mouse.getPosition()
-    table.insert(self.arrows, Arrow:new(World, self.x, self.y, mousex, mousey, self.fireStrength))
+    local playerx, playery = self.body:getWorldCenter()
+
+    table.insert(self.arrows, Arrow:new(World, playerx, playery, mousex, mousey, self.fireStrength))
+
     self.fireStrength = 0
     self.fireDelay = 1
   end
@@ -69,9 +76,25 @@ function Archer:update(dt)
 end
 
 function Archer:draw()
+  local cx, cy = self.body:getWorldCenter()
+  local mx, my = love.mouse.getPosition()
+  local halfw = self.w / 2
+  local angle = math.atan2((my - cy), (mx - cx)) + math.rad(45)
+
   for key, arrow in pairs(self.arrows) do arrow:draw() end
-  -- draw bow
-  love.graphics.circle('fill', self.x, self.y, 20)
+  love.graphics.circle('fill', cx, cy, 20)
+
+  local bowImage = self.bow.i0
+
+  if self.fireStrength > 1 / 2 then
+    bowImage = self.bow.i50
+  end
+
+  if self.fireStrength >= 1 then
+    bowImage = self.bow.i100
+  end
+
+  love.graphics.draw(bowImage, cx, cy, angle, 1, 1, halfw, halfw)
 end
 
 return Archer
